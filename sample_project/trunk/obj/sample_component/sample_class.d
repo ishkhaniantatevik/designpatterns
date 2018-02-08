66120a4dd74a0693e0028ee0ac024085a7fba6c0
U-MGC\hrtandil <hrtandil@amy-hrtandil-w7.mgc.mentorg.com>
2018-02-08 13:18:38 +0400

    - Removed sample projects compilation results, commited by mistake.

--- sample_project/trunk/obj/sample_component/sample_class.d
+++
@@ -1,3 +0,0 @@
-obj/sample_component/sample_class.o: \
- src/sample_component/sample_class.cpp \
- src/sample_component/sample_class.h
--- sample_project/trunk/obj/sample_project_main.d
+++
@@ -1,2 +0,0 @@
-obj/sample_project_main.o: src/sample_project_main.cpp \
- src/sample_component/sample_class.h
