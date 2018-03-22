bb706c6cec3036642e2128d39da78743cf921314
LusinehTsarukyan <tsarukyanlusine@gmail.com>
2018-03-22 09:03:05 -0700

    your message here

---
+++ 2018/lusine_tsarukyan/mainFactoryMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class mainFactoryMethod
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "BIG";
+            String s2 = "notBig";
+            BurgerCreatorBase creator = new BurgerCreator();
+            creator.makeBurger(s1);
+            creator.makeBurger(s2);
+        }
+    }
+}
